400589353a6d44c1857a5f7909f3fd6ef74621d2
hafiz-aa <mhafiz.aa@gmail.com>
2024-04-03 15:09:03 +0700

    Update map method

--- 07-count-by-x.js
+++ 07-count-by-x.js
@@ -1,7 +1,11 @@
+// function countBy(x, n) {
+//   let z = [];
+//   for (let i = 1; i <= n; i++) z.push(x * i);
+//   return z;
+// }
+
 function countBy(x, n) {
-  let z = [];
-  for (let i = 1; i <= n; i++) z.push(x * i);
-  return z;
+  return [...Array(n)].map((_, i) => x * (i + 1));
 }
 
-console.log(countBy(3, 9));
+console.log(countBy(3, 3));
